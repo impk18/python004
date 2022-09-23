9f7fd6920171a5a8792cf6fe15060b3d9a2e95d0
impk18 <113078334+impk18@users.noreply.github.com>
2022-09-23 18:59:41 -0500

    Add files via upload

---
+++ lab04.py
@@ -0,0 +1,125 @@
+import random as rndm
+
+def play_again() -> bool:
+
+    play_again_answer = input('Do you want to play again? Y/N ')
+
+    if play_again_answer == 'Y':
+
+        return True
+
+    if play_again_answer == 'N':
+
+        return False
+
+    else:
+
+        print('Sorry, try again')
+
+        play_again()
+
+
+def get_wager(bank : int) -> int:
+
+    user_wager = int(input('How many chips would you like to wager? '))
+
+    return user_wager
+
+def get_slot_results() -> tuple:
+
+    reela = rndm.randint(1,10)
+
+    reelb = rndm.randint(1,10)
+
+    reelc = rndm.randint(1,10)
+
+    return reela, reelb, reelc
+
+def get_matches(reela, reelb, reelc) -> int:
+
+    if reela == reelb == reelc:
+
+        matches = 3
+
+    elif reela == reelb and reela != reelc:
+
+        matches = 2
+
+    elif reelb == reelc and reelb != reela:
+
+        matches = 2
+
+    elif reelc == reela and reelc != reelb:
+
+        matches = 2
+
+    elif reela != reelb and reela != reelc and reelb != reelc:
+
+            matches = 0
+
+            return matches
+
+
+def get_bank() -> int:
+
+    bank = 1
+
+    bank = int(input('How many chips do you wanna play with? '))
+
+    if bank < 0 or bank > 100:
+
+        print('Sorry, try again')
+
+        get_bank()
+
+    return bank
+
+def get_payout(wager, matches):
+
+    if matches == 0:
+
+        return wager * -1
+
+    elif matches == 2:
+
+        return wager * 3
+
+    elif matches == 3:
+
+        return wager * 10
+
+if __name__ == "__main__":
+
+    playing = True
+
+    while playing:
+
+        bank = get_bank()
+
+        while bank > 0:
+
+            wager = get_wager(bank)
+
+            reel1, reel2, reel3 = get_slot_results()
+
+            matches = get_matches(reel1, reel2, reel3)
+
+            payout = get_payout(wager, matches)
+
+            bank = bank + payout
+
+            print("Your spin", reel1, reel2, reel3)
+
+            print("You matched", matches, "reels")
+
+            print("You won/lost", payout)
+
+            print("Current bank", bank)
+
+            print()
+
+            print("You lost all", 0, "in", 0, "spins")
+
+            print("The most chips you had was", 0)
+
+            playing = play_again()
